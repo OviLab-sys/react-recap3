5373e1f70ad43d7d0966e7d63d0cc3d744fefbf3
vicoduor <victoroduorr@gmail.com>
2025-03-17 13:31:04 +0300

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Alert } from './alert';
 
 function App() {
   return (
@@ -19,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Alert heading="success" closable>
+        Everything Looks good
+      </Alert>
     </div>
   );
 }
